f4ef36881bc4740ad5b16dfe0357c809a874a74a
nbchand <narenchand02@gmail.com>
2022-12-30 15:21:27 +0545

    feat: add exception handler

--- src/main/java/com/nabin/graphqlpractice/dtos/AuthorDto.java
+++ src/main/java/com/nabin/graphqlpractice/dtos/AuthorDto.java
@@ -3,6 +3,10 @@ package com.nabin.graphqlpractice.dtos;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 /**
  * @author Narendra
  * @version 1.0
@@ -11,6 +15,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AuthorDto {
+    @NotEmpty
+    @Pattern(regexp = "^[A-Za-z]{1,20}$", message = "Invalid name")
     private String name;
     private String thumbnail;
 }
---
+++ src/main/java/com/nabin/graphqlpractice/exceptions/CustomExceptionResolver.java
@@ -0,0 +1,33 @@
+package com.nabin.graphqlpractice.exceptions;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolationException;
+
+/**
+ * @author Narendra
+ * @version 1.0
+ * @since 2022-12-30
+ */
+@Component
+public class CustomExceptionResolver extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if (ex instanceof ConstraintViolationException) {
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.INTERNAL_ERROR)
+                    .message(ex.getMessage())
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        } else {
+            return null;
+        }
+    }
+}
